11e4aaa080cf993f5b87d3e45ce466fedcfba2e8
Lorenzo Drudi <lori.drudi.11@gmail.com>
2024-05-06 17:04:27 +0200

    fix: create movement directory.

--- reverie/backend_server/reverie.py
+++ reverie/backend_server/reverie.py
@@ -399,6 +399,8 @@ class ReverieServer:
           #  "persona": {"Klaus Mueller": {"movement": [38, 12]}}, 
           #  "meta": {curr_time: <datetime>}}
           curr_move_file = f"{sim_folder}/movement/{self.step}.json"
+          directory = os.path.dirname(curr_move_file)
+          os.makedirs(directory, exist_ok=True)
           with open(curr_move_file, "w") as outfile: 
             outfile.write(json.dumps(movements, indent=2))
 
