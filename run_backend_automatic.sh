b4f59aaba35d8ef01d6e51d069927eaf91220f39
Lorenzo Drudi <lori.drudi.11@gmail.com>
2024-05-15 23:43:15 +0200

    (scripts): improve backend scripts.

--- run_backend_automatic.sh
+++ run_backend_automatic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BACKEND_SCRIPT_PATH="reverie/backend_server"
-BACKEND_SCRIPT_FILE="reverie.py"
+BACKEND_SCRIPT_FILE="automatic_execution.py"
 CONDA_ENV="simulacra"
 LOGS_PATH="../../logs"
 
@@ -16,4 +16,4 @@ source /home/${USER}/anaconda3/bin/activate ${CONDA_ENV}
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "Timestamp: ${timestamp}"
 mkdir -p ${LOGS_PATH}
-python3 ${BACKEND_SCRIPT_FILE} --origin ${1} --target ${2} | tee  ${LOGS_PATH}/${2}_${timestamp}.txt
+python3 ${BACKEND_SCRIPT_FILE} --origin ${1} --target ${2} --steps ${3} | tee  ${LOGS_PATH}/${2}_${timestamp}.txt
