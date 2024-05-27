4811e900ef8d6039a6674e49360f1d3b78d1a963
Lorenzo Drudi <lori.drudi.11@gmail.com>
2024-05-27 18:06:26 +0200

    fix: no ui run.

--- reverie/backend_server/automatic_execution.py
+++ reverie/backend_server/automatic_execution.py
@@ -15,8 +15,6 @@ from datetime import datetime
 from multiprocessing import Process
 from openai_cost_logger import OpenAICostLoggerViz
 
-# Set the pid global variable
-pid = None
 
 def parse_args() -> Tuple[str, str, int, bool]:
     """Parse bash arguments
@@ -92,28 +90,26 @@ def get_starting_step(exp_name: str) -> int:
     return current_step
 
 
-def start_web_tab(ui: bool, browser_path: str, port: str) -> None:
+def start_web_tab(ui: bool, browser_path: str, port: str) -> int:
     """Open a new tab in the browser with the simulator home page
     
     Args:
         ui (bool): Open the simulator UI.
         browser_path (str): The path of the browser.
         port (str): The port number of the frontend server.
+        
+    Returns:
+        int: The process id of the web tab (only for headless chrome).
     """
     url = f"http://localhost:{port}/simulator_home"
     print("(Auto-Exec): Opening the simulator home page", flush=True)
     time.sleep(5)
+    pid = None
     try:
         if ui:
             webbrowser.get(browser_path).open(url, new=2)
         else:
-            # os.system(f"cpulimit -l 10 -- google-chrome\
-            #             --headless=new --no-sandbox --disable-dev-shm-usage --disable-extensions --disable-plugins\
-            #             --disable-accelerated-2d-canvas --no-first-run --single-process --no-zygote\
-            #             {url}")
-            
             command = [
-                "cpulimit", "-l", "20", "--",
                 "google-chrome",
                 "--headless=new",
                 "--no-sandbox",
@@ -130,6 +126,7 @@ def start_web_tab(ui: bool, browser_path: str, port: str) -> None:
             process = subprocess.Popen(command)
             pid = process.pid
             print(f"(Auto-Exec): Web tab process started with pid {pid}", flush=True)
+        return pid
     except Exception as e:
         print(e, flush=True)
 
@@ -170,7 +167,7 @@ def save_checkpoint(rs, idx: int, th: Process) -> Tuple[str, int, int]:
 
 if __name__ == '__main__':
     checkpoint_freq = 2 # 1 step = 10 sec
-    log_path = "cost-logs"
+    log_path = "cost-logs" # where the simulations' prints are stored
     idx = 0
     origin, target, tot_steps, ui, browser_path, port = parse_args()
     current_step = get_starting_step(origin)
@@ -192,8 +189,13 @@ if __name__ == '__main__':
             print(f"(Auto-Exec): STAGE {idx}", flush=True)
             print(f"(Auto-Exec): Running experiment '{exp_name}' from step '{current_step}' to '{curr_checkpoint}'", flush=True)
             rs = reverie.ReverieServer(origin, target)
-            th = Process(target=start_web_tab, args=(ui, browser_path, port))
-            th.start()
+            th, pid = None, None 
+            # Headless chrome doesn't need a thread since it create a dedicated thread by itself
+            if ui:
+                th = Process(target=start_web_tab, args=(ui, browser_path, port))
+                th.start()
+            else:
+                pid = start_web_tab(ui, browser_path, port)
             rs.open_server(input_command=f"run {steps_to_run}")
         except KeyboardInterrupt:
             print("(Auto-Exec): KeyboardInterrupt: Stopping the experiment.", flush=True)
@@ -204,7 +206,7 @@ if __name__ == '__main__':
             if step != 0:
                 origin, current_step, idx = save_checkpoint(rs, idx, th)
             else:
-                shutil.rmtree(f"../../environment/frontend_server/storage/{target}")
+                shutil.rmtree(f"../../environment/frontend_server/storage/{target}") # Remove the experiment folder if no steps were run
             print(f"(Auto-Exec): Error at step {current_step}", flush=True)
             print(f"(Auto-Exec): Exception {e.args[0]}", flush=True)
         else:
@@ -212,18 +214,16 @@ if __name__ == '__main__':
             curr_checkpoint = get_new_checkpoint(current_step, tot_steps, checkpoint_freq)
         finally:
             time.sleep(10) # Wait for the server to finish and then kill the process
-            if th.is_alive():
-                print(f"(Auto-Exec): Killing the web tab process", flush=True)
+            if th and th.is_alive():
                 th.kill()
                 th.join()
                 th.close()
                 gc.collect()
-                
-                if pid is not None:
-                    os.system(f"kill -9 {pid}")
-                    pid = None
-                
-                print(f"(Auto-Exec): Web tab process killed", flush=True)
+        
+            if pid:
+                os.system(f"kill -9 {pid}")
+                print(f"(Auto-Exec): Killed web tab process with pid {pid}", flush=True)
+                pid = None
 
     print(f"(Auto-Exec): EXPERIMENT FINISHED: {exp_name}")
     OpenAICostLoggerViz.print_experiment_cost(experiment=exp_name, path=log_path)
